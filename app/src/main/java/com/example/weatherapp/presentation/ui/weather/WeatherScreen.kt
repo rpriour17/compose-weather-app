1889f4b7ec8ed08ff9c629284b9b089c4baecf03
rpriour17 <rpriour17@gmail.com>
2022-12-02 11:50:25 -0800

    Progress on main weather card

--- app/src/main/java/com/example/weatherapp/presentation/ui/weather/WeatherScreen.kt
+++ app/src/main/java/com/example/weatherapp/presentation/ui/weather/WeatherScreen.kt
@@ -1,28 +1,27 @@
 package com.example.weatherapp.presentation.ui.weather
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.ColorUtils
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.viewModelFactory
-import dagger.hilt.android.lifecycle.HiltViewModel
+import com.example.weatherapp.R
+import com.example.weatherapp.presentation.ui.theme.WeatherAppTheme
 
 @Composable
 fun WeatherScreen() {
-    mainWeatherCard()
+    MainWeatherCard()
 }
 
 @Composable
-fun mainWeatherCard(
-    viewModel: WeatherScreenViewModel = hiltViewModel(),
+fun MainWeatherCard(
     modifier: Modifier = Modifier
 ) {
     Card(
@@ -36,10 +35,26 @@ fun mainWeatherCard(
                 .padding(16.dp)
         ) {
             Text(
-                text = "Today the pressure is ${
-                    viewModel.state.pressure
-                }"
+                text = "12:00",
+                modifier = Modifier.align(Alignment.End),
+                color = Color.White
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.d_113),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
         }
+    }
+}
+
+@Preview
+@Composable
+fun MainWeatherCardPreview() {
+    WeatherAppTheme {
+        MainWeatherCard()
     }
 }
